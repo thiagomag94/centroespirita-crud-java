9fc175e80ceb9d5f1e4c0fe9f6f6a2cecbb8bab0
thiagomag94 <thiagomag94@gmail.com>
2024-08-25 14:14:35 -0300

    first commit

---
+++ src/main/java/com/acervo/Acervo/AcervoApplication.java
@@ -0,0 +1,13 @@
+package com.acervo.Acervo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcervoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acervo/Acervo/AcervoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acervo.Acervo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
